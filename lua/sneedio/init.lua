4abbe04aaad46c2c28b31e6cd74efadb1f3ae8d9
msneed <matthewleesneed@gmail.com>
2023-11-11 19:49:23 -0600

    Initial

---
+++ lua/sneedio/init.lua
@@ -0,0 +1,12 @@
+require("packer")
+
+-- Main Configurations
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.cmd [[colorscheme habamax]]
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
---
+++ lua/sneedio/tabs.lua
@@ -0,0 +1,7 @@
+
+-- These are keymaps to create, close, and manuvure tabs
+--vim.keymap.set("n", "<leader>tt", vim.cmd.tabnew)
+--im.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
+--vim.keymap.set("n", "<leader>tp", vim.cmd.tabp)
+--vim.keymap.set("n", "<leader>tn", vim.cmd.tabn)
+--vim.keymap.set("n", "<leader>to", vim.cmd.tabo)
