0c2518cae0d88a18d1efd41af46401b7ceddc8e4
msneed <matthewleesneed@gmail.com>
2023-11-12 02:42:49 -0600

    Removing packer loop

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 
+-- Main Configurations
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.cmd [[colorscheme habamax]]
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+
+vim.keymap.set('i', '{}', '{}<C-o>i')
+vim.keymap.set('i', '{<CR>', '{<CR>}<C-o>O')
 
