27a2c922fe8d94b492b64d043baaeb583a41f61a
matthew.l.sneed.ctr <matthew.l.sneed.ctr@army.mil>
2024-06-27 15:20:09 -0500

    Adding auto-tag from Treesitter.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,31 +1,31 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
-    auto_install = true,
-    sync_install = false,
-    highlight = {
-        enable = true,
-        disable = { 'c', 'rust' }
-    },
-    indent = {
-        enable = true
-    },
-    autotag = {
-        enable = true,
-        disable = { 'c', 'rust' },
-        enable_rename = true,
-        enable_close = true,
-        enable_close_on_slash = true,
-    },
-    additional_vim_regex_highlighting = false,
+  ensure_installed = "all",
+  auto_install = true,
+  sync_install = false,
+  highlight = {
+    enable = true,
+    disable = { 'c', 'rust' }
+  },
+  indent = {
+    enable = true
+  },
+  autotag = {
+    enable = true,
+    disable = { 'c', 'rust' },
+    enable_rename = true,
+    enable_close = true,
+    enable_close_on_slash = true,
+    filetypes = { "html", "xml", "tsx", "jsx" }
+  },
+  additional_vim_regex_highlighting = false,
 }
 
--- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
--- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
---
--- local autotag = require("nvim-ts-autotag")
--- autotag.setup()
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+local autotag = require("nvim-ts-autotag")
+autotag.setup()
 
 local autopairs = require('nvim-autopairs')
 autopairs.setup({
-    disable_filetype = { "TelescopePrompt", "vim" }
+  disable_filetype = { "TelescopePrompt", "vim" }
 })
